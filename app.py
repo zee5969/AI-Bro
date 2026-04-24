ffbe25d3066e0942dc6c17362337470ac4646b3c
zee5969 <zee596923@gmail.com>
2026-04-24 19:03:06 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+from groq import Groq
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+
+st.set_page_config(page_title="My AI Chatbot", page_icon="🤖")
+st.title("🤖 My AI Chatbot")
+st.write("Ask me anything!")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+user_input = st.chat_input("Type your message here...")
+
+if user_input:
+    with st.chat_message("user"):
+        st.write(user_input)
+
+    st.session_state.messages.append({
+        "role": "user",
+        "content": user_input
+    })
+
+    response = client.chat.completions.create(
+        model="llama-3.1-8b-instant",
+        messages=st.session_state.messages
+    )
+
+    ai_reply = response.choices[0].message.content
+
+    with st.chat_message("assistant"):
+        st.write(ai_reply)
+
+    st.session_state.messages.append({
+        "role": "assistant",
+        "content": ai_reply
+    })
